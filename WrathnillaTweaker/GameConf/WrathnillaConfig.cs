39a874de10804fad9ab0867920d7290b70ca0297
Hukehrs Engineering <hel@nhdn.de>
2026-04-27 16:47:32 +0200

    chore: removed untweakable values

--- WrathnillaTweaker/GameConf/WrathnillaConfig.cs
+++ WrathnillaTweaker/GameConf/WrathnillaConfig.cs
@@ -4,5 +4,4 @@ public class WrathnillaConfig
 {
     public SkillConfig SkillConfig { get; set; } = new();
     public ExperienceConfig ExperienceConfig { get; set; } = new();
-    public MobConfig DamageConfig { get; set; } = new();
 }
